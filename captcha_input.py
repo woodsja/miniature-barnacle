5e84070db71a586f284fbe0a3a7a16d6da0e8c9e
woodsja <woodsja@gmail.com>
2018-01-10 15:34:58 -0500

    Add files via upload

---
+++ captcha_input.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+# queue up file names
+filename_queue = tf.train.string_input_producer(
+    tf.train.match_filenames_once("./training/*.png"))
+
+# read an image
+image_reader = tf.WholeFileReader()
+
+# read a file from the queue, ignoring the first value in the tuple
+_, image_file = image_reader.read(filename_queue)
+
+# decode image from PNG to a tensor
+image = tf.image.decode_png(image_file)
+
+# start a session with example output
+with tf.Session() as sess:
+    tf.global_variables_initializer()
+
+    image_tensor = sess.run([image])
+    print(image_tensor)
+
+    coord.request_stop()
+    coord.join(threads)
+
+
